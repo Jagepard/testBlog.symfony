f6b33a653c0168bbe8c5add6b7e8f4b0e49bd353
Jagepard <dankorot@gmail.com>
2024-04-02 11:12:19 +0300

    add auth

---
+++ bundle/Auth/src/Controller/RegistrationController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Bundle\Auth\Controller;
+
+use Bundle\Database\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Bundle\Auth\Form\RegistrationFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('admin.materials');
+        }
+
+        return $this->render('@Auth/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+}
