18d1092288d2cdd9b15173bd1c4d86eedbcf645c
Jagepard <dankorot@gmail.com>
2024-04-03 16:11:52 +0300

    add crud

--- bundle/Admin/src/Controller/MaterialsController.php
+++ bundle/Admin/src/Controller/MaterialsController.php
@@ -2,6 +2,7 @@
 
 namespace Bundle\Admin\Controller;
 
+use Bundle\Admin\Model\MaterialsModel;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,13 +25,46 @@ class MaterialsController extends AbstractController
         return $this->render('@Admin/materials/index.html.twig', ['materials' => $pagination]);
     }
 
-    #[Route('/material/add', name: 'add', methods: ['GET', 'POST'])]
-    public function add(Request $request): Response
+    #[Route('/material/add', name: 'add', methods: ['GET'])]
+    public function add(): Response
     {
-        if ($request->isMethod('post')) {
-            dd($request->request->all());
+        return $this->render('@Admin/materials/add.html.twig');
+    }
+
+    #[Route('/material/create', name: 'create', methods: ['POST'])]
+    public function create(Request $request, MaterialsModel $model): Response
+    {
+        if ($this->isCsrfTokenValid('add', $request->getPayload()->get('token'))) {
+            $model->create($request->request);
         }
 
-        return $this->render('@Admin/materials/add.html.twig');
+        return $this->redirectToRoute('admin.index');
+    }
+
+    #[Route('/material/edit/{id}', name: 'edit', methods: ['GET'])]
+    public function edit(MaterialsRepository $materialsRepository, $id): Response
+    {
+        $material = $materialsRepository->find($id);
+
+        return $this->render('@Admin/materials/edit.html.twig', [
+            'material' => $material
+        ]);
+    }
+
+    #[Route('/material/update/{id}', name: 'update', methods: ['POST'])]
+    public function update(Request $request, MaterialsModel $model, $id): Response
+    {
+        if ($this->isCsrfTokenValid('update', $request->getPayload()->get('token'))) {
+            $model->update($request->request, $id);
+        }
+
+        return $this->redirectToRoute('admin.index');
+    }
+
+    #[Route('/material/delete/{id}', name: 'delete', methods: ['GET'])]
+    public function delete(MaterialsModel $model, $id): Response
+    {
+        $model->delete($id);
+        return $this->redirectToRoute('admin.index');
     }
 }
